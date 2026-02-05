ebf570b4cbec0e037fb6c9e1050e4c33e08abe47
shimibe <shimibe@gmail.com>
2026-02-05 08:35:10 +0200

    layout fix

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -913,7 +913,7 @@ const Tasks = () => {
                     )}
                   </div>
                 </div>
-                <div className="col-span-2 grid grid-cols-5 gap-4">
+                <div className="col-span-2 grid grid-cols-3 gap-4">
                   <div>
                     <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
                       קטגוריה
@@ -960,7 +960,7 @@ const Tasks = () => {
                     </select>
                   </div>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="col-span-2 grid grid-cols-2 gap-4">
                   <div>
                     <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
                       משויך ל
